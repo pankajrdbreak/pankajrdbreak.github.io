0b55336d05c8259e672f4437f4d97c6bd59cc942
Pankaj Vare <pankaj.vare@samco.in>
2022-04-17 11:31:19 +0530

    adding

---
+++ my-app-master/src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -0,0 +1,23 @@
+package in.javahome.myweb.controller;
+import java.util.List;
+/*
+ * 
+ */
+public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j+10;
+	}
+	
+	public int reminder(int i, int j){
+		return i%j;
+	}
+	
+}
